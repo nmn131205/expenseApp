4b42a8885fdfab6fe10df33cb4317b403b76e2da
nmn131205 <njain123410@gmail.com>
2023-08-14 21:33:23 +0530

    Added my project

---
+++ client/src/components/Layout/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../styles/Footerstyle.css";
+
+const Footer = () => {
+  return (
+    <div id="footer" className=" bg-dark text-light p-4">
+      <h6 className="text-center text-white">
+        Made by Naman Jain
+      </h6>
+    </div>
+  );
+};
+
+export default Footer;
